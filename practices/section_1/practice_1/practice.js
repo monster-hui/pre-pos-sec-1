4fabdaa6204a4a370e51f6b213b1a6e238b44e81
monster-hui <373000945@qq.com>
2016-05-18 11:08:34 +0800

    add  function
    补充函数

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,13 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+	var a=collection_a.join(',');
+	var same_elements=[];
+	for(var i=0;i<collection_b.length;i++){
+		var form=new RegExp(collection_b[i]);
+		if(form.test(a)){
+		same_elements.push(collection_b[i]);
+		}
+	}
+	return same_elements
 }
 
 module.exports = collect_same_elements;
